31be0272d140ae80bc8c2cfc3fae65e174463f0e
hazellnut <tom.g.a.hazell@gmail.com>
2024-03-12 12:26:32 +1100

    Tidy code, collated tests

--- main.go
+++ main.go
@@ -16,35 +16,29 @@ import (
 var global_sudoku [9][9]int
 var result_sudoku [9][9]int
 
+var test = false
+
 func main() {
-	// runtime.SetCPUProfileRate((100000))
-	// f, err := os.Create("myprogram.prof")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// defer f.Close()
-	// pprof.StartCPUProfile(f)
-	// defer pprof.StopCPUProfile()
+	if test {
+		test_1()
+		return
+	}
+	sudokuWebServer()
+}
 
-	defer TimeTrack(time.Now())
-
-	// fmt.Println(quote.Go())
-
-	// var doggy Dog
-	// doggy.Animal = Animal{"Jason", 4}
-	// doggy.floof_score = 10
-
-	// fmt.Printf("This is a go test string, the dog's name is %s and he is so floofy: %d", doggy.Animal.name, doggy.floof_score)
-
+func sudokuWebServer() {
 	router := gin.Default()
 	router.LoadHTMLFiles("index.html")
-	// router.GET("/albums", getAlbums)
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 	router.POST("/solveSudoku", postSudoku)
 	router.Run("localhost:8080")
+}
+
+func test_1() {
+	defer TimeTrack(time.Now())
 	example_sudoku := [9][9]int{
 		{0, 0, 0, 0, 0, 0, 9, 0, 0},
 		{0, 0, 9, 0, 4, 3, 0, 8, 0},
@@ -57,9 +51,7 @@ func main() {
 		{0, 4, 0, 1, 5, 0, 7, 2, 0}}
 
 	new_arr := solveSudoku(example_sudoku)
-
 	print_sudoku(new_arr)
-
 }
 
 func TimeTrack(start time.Time) {
@@ -77,33 +69,6 @@ func TimeTrack(start time.Time) {
 
 	log.Println(fmt.Sprintf("%s took %s", name, elapsed))
 }
-
-// type album struct {
-// 	ID     string  `json:"id"`
-// 	Title  string  `json:"title"`
-// 	Artist string  `json:"artist"`
-// 	Price  float64 `json:"price"`
-// }
-
-// var albums = []album{
-// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-// 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-// 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-// }
-
-// type Animal struct {
-// 	name string
-// 	size int
-// }
-
-// type Dog struct {
-// 	Animal
-// 	floof_score int
-// }
-
-// func getAlbums(c *gin.Context) {
-// 	c.IndentedJSON(http.StatusOK, albums)
-// }
 
 func postSudoku(c *gin.Context) {
 
@@ -176,7 +141,9 @@ func solveCell(arr *[9][9]int, row int, col int) bool {
 
 func checkCell(arr *[9][9]int, row int, col int) bool {
 	value := arr[row][col]
+	//checking rows
 	for i := 0; i < 9; i++ {
+		//skip if current row
 		if i == row {
 			continue
 		}
@@ -184,7 +151,9 @@ func checkCell(arr *[9][9]int, row int, col int) bool {
 			return false
 		}
 	}
+	//checking columns
 	for i := 0; i < 9; i++ {
+		//skip if current column
 		if i == col {
 			continue
 		}
@@ -192,15 +161,19 @@ func checkCell(arr *[9][9]int, row int, col int) bool {
 			return false
 		}
 	}
+
+	//find current grid
 	gridcol := col / 3
 	gridrow := row / 3
 
 	//don't need to re-check columns
 	for i := 3 * gridcol; i < 3*(gridcol+1); i++ {
+		//skip if same column as cell
 		if i == col {
 			continue
 		}
 		for j := 3 * gridrow; j < 3*(gridrow+1); j++ {
+			//skip if same row as cell
 			if j == row {
 				continue
 			}
@@ -209,12 +182,13 @@ func checkCell(arr *[9][9]int, row int, col int) bool {
 			}
 		}
 	}
-
+	//no tests failed, we are good!
 	return true
 
 }
 
 func findNextEmptyCell(arr [9][9]int, row int, col int) (int, int) {
+	//find next cell that has "0" in it
 	for i := col; i < 9; i++ {
 		for j := 0; j < 9; j++ {
 			if i == col && j < row {
